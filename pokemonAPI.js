d734ca38d4b77ef4488d12725790863bcf97207b
grillz <grillz@grillzs-MacBook-Pro.local>
2019-10-15 10:32:05 +0200

    update

--- pokemonAPI.js
+++ pokemonAPI.js
@@ -3,3 +3,32 @@ const getPokemons = axios.create({
 });
 
 export default getPokemons;
+
+
+
+let product = {
+  name: "headphones",
+  price: 83.7,
+  //discount: "7%",
+};
+​
+​
+let hasDiscount = false;
+​
+for(let key in product){
+​ if(key === "discount") hasDiscount = true;
+}
+
+if(hasDiscount){ // check if "discount" exists
+console.log(`Already discounted by ${product.discount}`); // if yes print message
+}else {                          // if "discount doesn't exist"
+if (product.price > 100) {
+  product.discount = 10;
+  product.price = product.price * 0.90;
+}
+else {
+  product.discount = 7;
+  product.price = product.price * 0.93;
+}
+console.log(product);
+}
