67c2f07cfb88b5f228b809aa060223bfda0df8cb
yeeeehaw <frankomarmier@gmail.com>
2020-09-29 11:41:45 +0200

    Remove out of context code

--- pokemonAPI.js
+++ pokemonAPI.js
@@ -1,34 +1,5 @@
 const getPokemons = axios.create({
-  baseURL: "https://pokeapi.co/api/v2/pokemon"
+  baseURL: "https://pokeapi.co/api/v2/pokemon",
 });
 
 export default getPokemons;
-
-
-
-let product = {
-  name: "headphones",
-  price: 83.7,
-  //discount: "7%",
-};
-​
-​
-let hasDiscount = false;
-​
-for(let key in product){
-​ if(key === "discount") hasDiscount = true;
-}
-
-if(hasDiscount){ // check if "discount" exists
-console.log(`Already discounted by ${product.discount}`); // if yes print message
-}else {                          // if "discount doesn't exist"
-if (product.price > 100) {
-  product.discount = 10;
-  product.price = product.price * 0.90;
-}
-else {
-  product.discount = 7;
-  product.price = product.price * 0.93;
-}
-console.log(product);
-}
